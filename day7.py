e84e0c9a5208a76de022e0e4aad5e66f0eaac0df
Rajendar <rajendarbore321@gmail.com>
2025-11-10 08:12:02 +0000

    new code

---
+++ day7.py
@@ -0,0 +1,37 @@
+# read 2 numbers from user and print sum of numbers
+num1 = input ("enter the 1st number")
+
+num2 = input ("enter the 2nd number")
+
+
+# convert str --> float 
+
+a =float (num1)
+b =float (num2)
+
+print(a+b)
+
+
+# read 3 float nad 1 int numbers from user sum of 3 floats 1 int
+
+num1 =input("enter 1st numer :")
+
+num2 =input("enter 2nd number :")
+
+num3 =input("enter 3rd number :")
+
+num4 =input("enter 4th number :")
+
+#convert str --> float
+
+a =float(num1)
+b =float(num2)
+c =float(num3)
+d =int(num4)
+
+print(a+b+c-d)
+
+
+
+
+
